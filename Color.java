4d67edb0dbd2feeb41d7298da180c06405627e81
Faiz Ahmed <41271304+faizahmed2018@users.noreply.github.com>
2018-07-24 18:42:16 -0700

    Create Color.java

---
+++ Color.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class homework2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		favColor("red"); 
+		
+
+	}
+	 
+	public static void favColor(String color) {
+	if (color == "red")
+	{
+	System.out.println("red");
+	}	
+	else if (color == "green")
+	{
+	System.out.println("green");	
+	}
+	else if (color == "purple")
+	{
+		System.out.println("purple");		
+	}
+	else {
+		System.out.println("we don't have that color.");		
+	}
+	
+	
+	}
+}
