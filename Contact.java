69ee59a3cae34fea5d29afb21b6f95d965ddafe0
Faiz Ahmed <41271304+faizahmed2018@users.noreply.github.com>
2018-07-29 10:54:25 -0700

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -2,22 +2,21 @@
 public class homeWork {
 
 	public static void main(String[] args) {
-		System.out.println((newContact("Farhad " , 42599999)));
-		System.out.println((newcontact("unknown")));
-		
+		System.out.println((newContact("Farhad", "(425) 999-9999")));
+		System.out.println((newcontact("Faiz")));
+
 	}
 
-	public static String newContact(String name , int phoneNum) {
-		
-		return name + phoneNum ;
-		
+	public static String newContact(String name, String phoneNum) {
+
+		return name + " " + phoneNum;
+
 	}
+
 	public static String newcontact(String name) {
 
-		
-		return name;
-	
-	}
-	
-}
+		return newContact(name, "unknown");
 
+	}
+
+}
